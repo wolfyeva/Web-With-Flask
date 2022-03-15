4d68d8dc47adccea701bc7aee787be8410507bfb
Benjamin <benctw@gmail.com>
2022-03-15 18:01:29 +0800

    更新模組2範例

--- Module_02/2_app_w_variable.py
+++ Module_02/2_app_w_variable.py
@@ -4,26 +4,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    a=1/0
     return 'Hello, Flask!'
 
 @app.route('/user/<username>')
 def show_user_profile(username):
-    # show the user profile for that user
-    return '<h1>Hello, %s^^<h1>' % escape(username)
+    # 顯示使用者的個人資料，如名字
+    return '<h1>Hello, %s</h1>' % escape(username)
 
 @app.route('/user')
 def user_page():
-    return '請在網址輸入名稱'
+    return '<h1>現在進入的是 /user 路由</h1>'
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
+    # 顯示文章編號，文章編號是整數
+    return f'文章編號為：{post_id}'
+
+@app.route('/temp/<float:temp_value>')
+def show_post(temp_value):
+    # 顯示溫度數值，溫度是浮點數
+    return f'現在溫度為：{temp_value}'
 
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
-    # show the subpath after /path/
+    # 顯示包含 '/' 符號的字串，視為路徑(path)
     return 'Subpath %s' % escape(subpath)
     
 if __name__ == "__main__":
